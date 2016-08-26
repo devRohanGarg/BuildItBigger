51a8d2085875035116e5eb8760d4567e15b6a5a0
Rohan Garg <dev.RohanGarg@gmail.com>
2016-08-26 17:11:48 +0530

    Add JokeViewer activity

---
+++ jokeViewer/src/main/java/com/udacity/gradle/builditbigger/jokeviewer/JokeViewerActivityFragment.java
@@ -0,0 +1,25 @@
+package com.udacity.gradle.builditbigger.jokeviewer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class JokeViewerActivityFragment extends Fragment {
+
+    public JokeViewerActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_joke_viewer, container, false);
+
+        return root;
+    }
+}
